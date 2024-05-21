a46438e475ab81c52e2955ad431057fae88ffe6a
JasonEvan <7ason3van@gmail.com>
2024-05-21 19:13:04 +0700

    menyelesaikan responsive navbar

--- script.js
+++ script.js
@@ -1,42 +1,72 @@
-const homePage = document.getElementById('home-page');
-const aboutPage = document.getElementById('about-page');
-const productPage = document.getElementById('product-page');
-const testiPage = document.getElementById('testi-page');
+const homePage = document.querySelectorAll('.home-page');
+const aboutPage = document.querySelectorAll('.about-page');
+const productPage = document.querySelectorAll('.product-page');
+const testiPage = document.querySelectorAll('.testi-page');
 const registerPage = document.getElementById('register');
+const navbarResponsive = document.getElementById('navbar-responsive');
 
-homePage.addEventListener('click', () => {
-    if (window.location.pathname.includes("/web-HCI")) {
-        window.location.href = '/web-HCI/index.html';
-    }
-    window.location.href = '/index.html';
+homePage.forEach((element) => {
+    element.addEventListener('click', () => {
+        if (window.location.pathname.includes("/web-HCI/")) {
+            window.location.href = '/web-HCI/index.html';
+        } else {
+            window.location.href = '/index.html';
+        }
+    });
 });
 
-aboutPage.addEventListener('click', () => {
-    if (window.location.pathname.includes("/web-HCI")) {
-        window.location.href = '/web-HCI/page/about.html';
-    }
-    window.location.href = '/page/about.html';
+aboutPage.forEach((element) => {
+    element.addEventListener('click', () => {
+        if (window.location.pathname.includes("/web-HCI/")) {
+            window.location.href = '/web-HCI/page/about.html';
+        } else {
+            window.location.href = '/page/about.html';
+        }
+    });
 });
 
-productPage.addEventListener('click', () => {
-    if (window.location.pathname.includes("/web-HCI")) {
-        window.location.href = '/web-HCI/page/product.html';
-    }
-    window.location.href = '/page/product.html';
+productPage.forEach((element) => {
+    element.addEventListener('click', () => {
+        if (window.location.pathname.includes("/web-HCI/")) {
+            window.location.href = '/web-HCI/page/product.html';
+        } else {
+            window.location.href = '/page/product.html';
+        }
+    });
 });
 
-testiPage.addEventListener('click', () => {
-    if (window.location.pathname.includes("/web-HCI")) {
-        window.location.href = '/web-HCI/page/testimoni.html';
-    }
-    window.location.href = '/page/testimoni.html';
+testiPage.forEach((element) => {
+    element.addEventListener('click', () => {
+        if (window.location.pathname.includes("/web-HCI/")) {
+            window.location.href = '/web-HCI/page/testimoni.html';
+        } else {
+            window.location.href = '/page/testimoni.html';
+        }
+    });
 });
 
 registerPage.addEventListener('click', () => {
-    if (window.location.pathname.includes("/web-HCI")) {
+    if (window.location.pathname.includes("/web-HCI/")) {
         window.location.href = '/web-HCI/page/register.html';
+    } else {
+        window.location.href = '/page/register.html';
     }
-    window.location.href = '/page/register.html';
+});
+
+document.getElementById("register-menu").addEventListener("click", () => {
+    if (window.location.pathname.includes("/web-HCI/")) {
+        window.location.href = '/web-HCI/page/register.html';
+    } else {
+        window.location.href = '/page/register.html';
+    }
+});
+
+document.getElementById("menu-icon").addEventListener("click", () => {
+    navbarResponsive.style.display = "flex";
+});
+
+document.getElementById("silang").addEventListener("click", () => {
+    navbarResponsive.style.display = "none";
 });
 
 document.getElementById("ig").addEventListener("click", () => {
